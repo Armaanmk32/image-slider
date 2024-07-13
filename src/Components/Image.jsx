e3bb28ed3a1ad980a88283eaa96ba571a5f749fe
Armaanmk32 <abdirahmanabdallanasser@gmail.com>
2024-07-13 11:32:45 +0300

    waxaan sameyney image slider

---
+++ src/Components/Image.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+
+function Image(){
+
+
+
+    const[index, setIndex]= useState(0)
+    
+
+    const handleNext = ()=>{
+        if(index < images.length -1){
+        setIndex(index + 1)
+        }
+    }
+    const handlePrev = ()=>{
+        if(index > 0){
+
+            setIndex(index - 1)
+
+        }
+        
+    }
+
+
+
+    const images =["https://images.pexels.com/photos/2564889/pexels-photo-2564889.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+
+
+        "https://images.pexels.com/photos/6658230/pexels-photo-6658230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+
+
+        "https://images.pexels.com/photos/1213921/pexels-photo-1213921.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+
+
+        "https://images.pexels.com/photos/11630697/pexels-photo-11630697.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+    ]
+
+
+
+
+
+
+
+
+    return <div className=" sm:flex text-center">
+        <div className="flex justify-center">
+            <img className="bg-orange-500 sm:w-[900px] w-[350px] object-cover sm:h-[390px] rounded-xl mt-10" src= {images[index]} />
+        </div>
+        <button onClick={handleNext} className="bg-sky-500 h-10 w-24 mt-16  sm:px-16 rounded sm:py-4 m-5 text-white text-3xl">Next</button>
+        <button onClick={handlePrev} className="bg-sky-500 h-10 w-24 mt-16  sm:px-16 rounded sm:py-4 m-5 text-white text-3xl">Prev</button>
+    </div>
+}
+
+export default Image
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import "./Style.css"
+import Image from './Components/Image'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+
+
+<div>
+  <Image />
+</div>
+)
